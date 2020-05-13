753ad2361763818b1801ab2d66663628372f0bbc
polinakhari <polina14.05@yandex.ru>
2020-05-13 21:02:00 +0300

    first

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const cors = require('cors')
+const moment = require('moment')
 
 const PORT = process.env.PORT || 5000
 
@@ -20,20 +21,20 @@ io.on('connection', (socket) => {
         const {error, user} = addUser({id:socket.id, name, room});
         
         if (error) callback(error);
-        console.log(user)
-        socket.join(user.room);
+        
+        socket.join(user.room);        
 
-        socket.emit('message', {user: "admin", text: `${user.name}, welcome to the room ${user.room}`})
-        socket.broadcast.to(user.room).emit("message", {user : "admin", text: `${user.name} has joined!`})
+        socket.emit('message', {user: "admin", text: `${user.name}, welcome to the room ${user.room}`, time: moment().format('HH:mm') })
+        socket.broadcast.to(user.room).emit("message", {user : "admin", text: `${user.name} has joined!`, time: moment().format('HH:mm')})
           
         io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
 
         callback();
     })
 
-    socket.on("sendMessage", (message, callback) => {
+    socket.on("sendMessage", (message, callback) => {        
         const user = getUser(socket.id)
-        io.to(user.room).emit("message", {user: user.name, text: message})
+        io.to(user.room).emit("message", {user: user.name, text: message, time: moment().format('HH:mm')})
         io.to(user.room).emit("roomData", {room: user.room,  users: getUsersInRoom(user.room)})
         callback()
     })
@@ -42,7 +43,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
 
         if(user) {
-            io.to(user.room).emit("message", {user: "admin", text: `${user.name} has left`}, )
+            io.to(user.room).emit("message", {user: "admin", text: `${user.name} has left`, time: moment().format('HHH:mm')}, )
         }
     } )
 })
